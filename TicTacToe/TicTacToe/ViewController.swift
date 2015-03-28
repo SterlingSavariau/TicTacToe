d6ca921202257e3738a06844889968a576b11fb5
SterlingSavariau <Sterling.Savariau@gmail.com>
2015-03-28 15:26:28 -0400

    Connected MPC and modified ViewController.

--- TicTacToe/TicTacToe/ViewController.swift
+++ TicTacToe/TicTacToe/ViewController.swift
@@ -7,13 +7,49 @@
 //
 
 import UIKit
+import MultipeerConnectivity
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MCBrowserViewControllerDelegate {
 
+    @IBOutlet var fields: [TTTImageView]!
+    
+    var appDelegate:AppDelegate!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        appDelegate = UIApplication.sharedApplication().delegate as AppDelegate
+        appDelegate.mpcHandler.setupPeerWithDisplayName(UIDevice.currentDevice().name)
+        appDelegate.mpcHandler.setupSession()
+        appDelegate.mpcHandler.advertiseSelf(true)
     }
+    
+    @IBAction func connectWithPlayer(sender: AnyObject) {
+        if appDelegate.mpcHandler.session != nil{
+            appDelegate.mpcHandler.setupBrowser()
+            appDelegate.mpcHandler.browser.delegate = self
+            
+            self.presentViewController(appDelegate.mpcHandler.browser, animated: true, completion: nil)
+            
+        }
+    }
+    
+    func setupGameLogic (){
+        for index in 0 ... fields.count - 1{
+            let gestureRecognizer = UITapGestureRecognizer(target: self, action: "fieldTapped:")
+            gestureRecognizer.numberOfTapsRequired = 1
+            
+            fields[index].addGestureRecognizer(gestureRecognizer)
+        }
+    }
+    
+    func browserViewControllerDidFinish(browserViewController: MCBrowserViewController!) {
+        appDelegate.mpcHandler.browser.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func browserViewControllerWasCancelled(browserViewController: MCBrowserViewController!) {
+        appDelegate.mpcHandler.browser.dismissViewControllerAnimated(true, completion: nil)
+    }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
